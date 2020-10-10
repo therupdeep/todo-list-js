6466dee301f90b70ce0c2b8166164cc20ee98fb0
RupdeepSaha <rupdeep176@hotmail.com>
2020-10-11 02:47:14 +0530

    Todo can be added

--- app.js
+++ app.js
@@ -10,12 +10,30 @@ todoButton.addEventListener('click',addTodo);
 
 //Functions
 
-const addTodo = (event) => {
+function addTodo(event)
+{
     //Prevent Form Submission
     event.preventDefault();
     //Todo Div
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
     //Create List
-    const newTodo
+    const todoItem = document.createElement("li");
+    todoItem.innerText = todoInput.value;
+    todoItem.classList.add("todo-item");
+    todoDiv.appendChild(todoItem);
+    //Checked button
+    const checkedButton = document.createElement("button");
+    checkedButton.innerHTML = '<i class="fas fa-check"></i>';
+    checkedButton.classList.add("checked-btn");
+    todoDiv.appendChild(checkedButton);
+    //Delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteButton.classList.add("delete-btn");
+    todoDiv.appendChild(deleteButton);
+    //Append to List
+    todoList.appendChild(todoDiv);
+    //Clear todo input value
+    todoInput.value = "";
 }
